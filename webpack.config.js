836c8673c049cd7c00fa5b32f33cc4565a81308b
plainheart <yhen@all-my-life.cn>
2020-09-25 18:13:45 +0800

    [dev] update build configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-var webpack = require("webpack");
-var isProd = process.argv.indexOf("-p") > -1;
+const webpack = require('webpack');
+const ENTRY_NAME = 'gmap';
 
-module.exports = {
-  entry: {
-    'gmap': __dirname + "/src/index.js"
-  },
-  output: {
-    libraryTarget: "umd",
-    library: ["[name]"],
-    path: __dirname + "/dist",
-    filename: "echarts-extension-" + (isProd ? "[name].min.js" : "[name].js")
-  },
-  devtool: isProd ? "source-map" : "cheap-module-source-map",
-  externals: {
-    echarts: "echarts"
+module.exports = function (env, argv) {
+  const isProd = env === 'production';
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: {
+      [ENTRY_NAME]: __dirname + '/src/index.js'
+    },
+    output: {
+      libraryTarget: 'umd',
+      library: ['echarts', ENTRY_NAME],
+      umdNamedDefine: true,
+      globalObject: 'this',
+      path: __dirname + '/dist',
+      pathinfo: !isProd,
+      filename: 'echarts-extension-' + (isProd ? '[name].min.js' : '[name].js')
+    },
+    devtool: isProd ? false : 'source-map',
+    externals: {
+      echarts: 'echarts'
+    }
   }
 }
